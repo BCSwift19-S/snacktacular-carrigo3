5ba7aa7c46520099ae42a9fddebd13e847efd797
Cam Arrigo <carrigo3@gmail.com>
2019-04-22 13:03:11 -0400

    9.19 Completed

--- Snacktacular/Photo.swift
+++ Snacktacular/Photo.swift
@@ -5,7 +5,7 @@
 //  Created by Cameron Arrigo on 4/14/19.
 //  Copyright © 2019 John Gallaugher. All rights reserved.
 //
-
+import UIKit
 import Foundation
 import Firebase
 
@@ -13,14 +13,14 @@ class Photo {
     var image: UIImage
     var description: String
     var postedBy: String
-    var date: Date
+    var date: TimeInterval
     var documentUUID: String // Universal Unique Identifier
     
     var dictionary: [String: Any] {
         return ["description": description, "postedBy": postedBy, "date": date]
     }
     
-    init(image: UIImage, description: String, postedBy: String, date: Date, documentUUID: String) {
+    init(image: UIImage, description: String, postedBy: String, date: TimeInterval, documentUUID: String) {
         self.image = image
         self.description = description
         self.postedBy = postedBy
@@ -30,7 +30,15 @@ class Photo {
     
     convenience init() {
         let postedBy = Auth.auth().currentUser?.email ?? "Unknown User"
-        self.init(image: UIImage(), description: "", postedBy: postedBy, date: Date(), documentUUID: "")
+        let newDate = Date().timeIntervalSince1970
+        self.init(image: UIImage(), description: "", postedBy: postedBy, date: newDate, documentUUID: "")
+    }
+    
+    convenience init(dictionary: [String: Any]) {
+        let description = dictionary["description"] as! String? ?? ""
+        let postedBy = dictionary["postedBy"] as! String? ?? ""
+        let date = dictionary["date"] as! TimeInterval? ?? 0.0
+        self.init(image: UIImage(), description: "", postedBy: postedBy, date: date, documentUUID: "")
     }
     
     func saveData(spot: Spot, completed: @escaping (Bool) -> ()) {
@@ -41,10 +49,19 @@ class Photo {
             print("*** ERROR: could not convert image to data format")
             return completed(false)
         }
+        let uploadMetadata = StorageMetadata()
+        uploadMetadata.contentType = "image/jpeg"
         documentUUID = UUID().uuidString //Generate a unique ID to use for the photo image's name
         // create a ref to upload storage to spot.documentID's folder (bucket), with the name we created.
         let storageRef = storage.reference().child(spot.documentID).child(self.documentUUID)
-        let uploadTask = storageRef.putData(photoData)
+        let uploadTask = storageRef.putData(photoData, metadata: uploadMetadata)
+        {metadata, error in
+            guard error == nil else {
+                print("*** ERROR: Error during .putData storage upload for \(storageRef). Error: \(error?.localizedDescription)")
+                return
+            }
+            print("UPLOAD WORKED! Metadata is \(metadata)")
+        }
         
         uploadTask.observe(.success) { (snapshot) in
             let dataToSave = self.dictionary
